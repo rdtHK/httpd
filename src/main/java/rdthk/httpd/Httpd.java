1ee1528d76e627356412a625d41bc60b9e63b45c
rdtHK <mariop.36754@gmail.com>
2018-08-06 23:19:25 -0300

    Works but is missing a few tests and command line options.

---
+++ src/main/java/rdthk/httpd/Httpd.java
@@ -0,0 +1,71 @@
+package rdthk.httpd;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Httpd {
+    private final RequestHandler requestHandler;
+    private boolean running;
+
+    public static void main(String[] args) {
+        Httpd httpd = new Httpd(new RequestHandler(new FileLoader("src/test/resources")));
+        httpd.start(3000);
+    }
+
+    public Httpd(RequestHandler requestHandler) {
+        this.requestHandler = requestHandler;
+    }
+
+    public void start(int port) {
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(port);
+            running = true;
+        } catch (IOException e) {
+            throw new HttpdException(e);
+        }
+
+        while (running) {
+            try {
+                Socket client = server.accept();
+                ConnectionHandler connectionHandler = new ConnectionHandler(requestHandler, client);
+                connectionHandler.start();
+            } catch (IOException e) {
+                throw new HttpdException(e);
+            }
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    private class ConnectionHandler extends Thread {
+        private final Socket socket;
+        private final RequestHandler requestHandler;
+
+        public ConnectionHandler(RequestHandler requestHandler, Socket socket) {
+            this.requestHandler = requestHandler;
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                Request request = new Request(socket.getInputStream());
+                Response response = new Response(socket.getOutputStream());
+
+                requestHandler.handleRequest(request, response);
+            } catch (IOException e) {
+                throw new HttpdException(e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new HttpdException(e);
+                }
+            }
+        }
+    }
+}
